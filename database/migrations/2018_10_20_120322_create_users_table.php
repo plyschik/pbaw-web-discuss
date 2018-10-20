ab63e5aa9d4ad2cd47435a1b17c07c7a87801101
plyschik <palyschik@gmail.com>
2018-10-20 12:23:53 +0200

    User model update.

--- database/migrations/2018_10_20_120322_create_users_table.php
+++ database/migrations/2018_10_20_120322_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+            $table->mediumIncrements('id');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name', 32);
+            $table->string('last_name', 32);
             $table->rememberToken();
             $table->timestamps();
         });
