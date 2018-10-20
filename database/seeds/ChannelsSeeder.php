587b65466b15c07dd33975c077946dcec67ef9ab
emicsto <emicsto@gmail.com>
2018-10-20 21:27:54 +0200

    Channels seeder added.

---
+++ database/seeds/ChannelsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Channel;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ChannelsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            Channel::create([
+                'name' => $faker->unique()->word
+            ]);
+        }
+//        Channel::create([
+//            'name' => $faker->word
+//        ]);
+    }
+}
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             UsersSeeder::class,
-            TopicsSeeder::class
+            TopicsSeeder::class,
+            ChannelsSeeder::class
         ]);
     }
 }
