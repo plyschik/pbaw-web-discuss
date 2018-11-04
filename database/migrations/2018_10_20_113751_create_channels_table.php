8d05879f26475f5df261be1c3f3e18e85c9beab0
plyschik <palyschik@gmail.com>
2018-11-04 23:13:26 +0100

    Channel slug added.

--- database/migrations/2018_10_20_113751_create_channels_table.php
+++ database/migrations/2018_10_20_113751_create_channels_table.php
@@ -15,7 +15,9 @@ class CreateChannelsTable extends Migration
     {
         Schema::create('channels', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('slug', 32);
             $table->string('name', 32);
+            $table->string('description', 128);
             $table->timestamps();
         });
     }
