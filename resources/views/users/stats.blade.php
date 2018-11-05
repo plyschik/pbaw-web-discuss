195e3cd4c9c1b58c9571f7f8ff0b2d77603c6ed1
emicsto <emicsto@gmail.com>
2018-11-05 22:54:45 +0100

    Statistics view fixed.

--- resources/views/users/stats.blade.php
+++ resources/views/users/stats.blade.php
@@ -2,17 +2,27 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-header">Statistics</div>
-            <div class="card-body">
-                <div class="row mb-3">
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-header">
-                                Age of users
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">Statistics</div>
+                    <div class="card-body">
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <div class="card-header">Age of users</div>
+                                    <div class="card-body">
+                                        {!! $ageChart->container() !!}
+                                    </div>
+                                </div>
                             </div>
-                            <div class="card-body">
-                                {!! $ageChart->container() !!}
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <div class="card-header">Channels</div>
+                                    <div class="card-body">
+                                        {!! $channelChart->container() !!}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
@@ -24,34 +34,13 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        Channels
-                                    </div>
-                                    <div class="card-body">
-                                        {!! $channelChart->container() !!}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-header">
-                                        Users activity due to time
-                                    </div>
-                                    <div class="card-body">
-                                        {!! $activityChart->container() !!}
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js"
-                    charset="utf-8"></script>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js" charset="utf-8"></script>
     {!! $ageChart->script() !!}
     {!! $channelChart->script() !!}
     {!! $activityChart->script() !!}
