cb23c114076db41c1882e0b55cab0e485d6fd401
emicsto <emicsto@gmail.com>
2018-11-05 22:59:33 +0100

    Counter on the channels view fixed.

--- resources/views/channels/index.blade.php
+++ resources/views/channels/index.blade.php
@@ -23,34 +23,40 @@
                                 </div>
 
                                 @hasrole('administrator')
-                                    <div class="row mt-3">
+                                <div class="row mt-3">
+                                    <div class="col-2">
+                                        <a class="btn btn-sm btn-block btn-outline-primary"
+                                           href="{{ route('channels.edit', $channel) }}">
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </a>
+                                    </div>
+                                    @if ($channel->topics_count == 0)
                                         <div class="col-2">
-                                            <a class="btn btn-sm btn-block btn-outline-primary" href="{{ route('channels.edit', $channel) }}">
-                                                <i class="fas fa-pencil-alt"></i>
-                                            </a>
-                                        </div>
-                                        @if ($channel->topics_count == 0)
-                                            <div class="col-2">
-                                                <form class="form-inline" action="{{ route('channels.destroy', $channel) }}" method="POST">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button class="btn btn-sm btn-block btn-outline-danger confirm-delete" type="submit">
-                                                        <i class="far fa-trash-alt"></i>
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        @else
-                                            <div class="col-2">
-                                                <button class="btn btn-sm btn-block btn-outline-danger" data-toggle="tooltip" data-placement="top" title="You can only delete channel without topics." disabled="disabled">
+                                            <form class="form-inline" action="{{ route('channels.destroy', $channel) }}"
+                                                  method="POST">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button class="btn btn-sm btn-block btn-outline-danger confirm-delete"
+                                                        type="submit">
                                                     <i class="far fa-trash-alt"></i>
                                                 </button>
-                                            </div>
-                                        @endif
-                                    </div>
+                                            </form>
+                                        </div>
+                                    @else
+                                        <div class="col-2">
+                                            <button class="btn btn-sm btn-block btn-outline-danger"
+                                                    data-toggle="tooltip" data-placement="top"
+                                                    title="You can only delete channel without topics."
+                                                    disabled="disabled">
+                                                <i class="far fa-trash-alt"></i>
+                                            </button>
+                                        </div>
+                                    @endif
+                                </div>
                                 @endhasrole
                             </td>
                             <td class="text-center align-middle">{{ $channel->topics_count }}</td>
-                            <td class="text-center align-middle">{{ --$channel->replies_count }}</td>
+                            <td class="text-center align-middle">{{ $channel->replies_count>0 ? --$channel->replies_count : 0}}</td>
                             <td class="small align-middle">
                                 @if ($channel->lastReplies->isEmpty())
                                     ---
@@ -59,7 +65,8 @@
                                         <a href="{{ route('topics.show', ['topic' => $channel->lastReplies->first()['topic']['id']]) }}">{{ str_limit($channel->lastReplies->first()['topic']['title'], 20) }}</a>
                                     </div>
                                     <div clas="d-block">
-                                        Author: <a href="{{ route('users.show', ['user' => $channel->lastReplies->first()['user']['id']]) }}">{{ $channel->lastReplies->first()['user']['name'] }}</a>
+                                        Author: <a
+                                                href="{{ route('users.show', ['user' => $channel->lastReplies->first()['user']['id']]) }}">{{ $channel->lastReplies->first()['user']['name'] }}</a>
                                     </div>
                                     <div class="d-block">
                                         <div class="text-muted">
@@ -124,13 +131,16 @@
                             Average age of users: {{ $stats['users']['average_age'] }}
                         </li>
                         <li class="list-group-item">
-                            Last registered: <a href="{{ route('users.show', $stats['users']['last_registered']['id']) }}">{{ $stats['users']['last_registered']['name'] }}</a>
+                            Last registered: <a
+                                    href="{{ route('users.show', $stats['users']['last_registered']['id']) }}">{{ $stats['users']['last_registered']['name'] }}</a>
                         </li>
                         <li class="list-group-item">
-                            Last logged in: <a href="{{ route('users.show', $stats['users']['last_logged_in']['id']) }}">{{ $stats['users']['last_logged_in']['name'] }}</a>
+                            Last logged in: <a
+                                    href="{{ route('users.show', $stats['users']['last_logged_in']['id']) }}">{{ $stats['users']['last_logged_in']['name'] }}</a>
                         </li>
                         <li class="list-group-item">
-                            The most replies ({{ $stats['most_replies']['total'] }}) were on {{ $stats['most_replies']['date'] }}
+                            The most replies ({{ $stats['most_replies']['total'] }}) were
+                            on {{ $stats['most_replies']['date'] }}
                         </li>
                     </ul>
                 </div>
