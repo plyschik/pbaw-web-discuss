6321659161c726a585aa87176abec05cad190cf2
plyschik <palyschik@gmail.com>
2018-11-07 18:58:48 +0100

    Code cleanup.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -97,7 +97,6 @@
 @stop
 
 @section('content')
-    {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>--}}
     <div class="container">
         <div class="row">
             <div class="col-8">
