9b83d4ff574627db95ec0a858be9a574c9a4b529
emicsto <emicsto@gmail.com>
2018-11-17 17:05:47 +0100

    Link to the category in the create moderator view deleted.

--- resources/views/users/create_moderator.blade.php
+++ resources/views/users/create_moderator.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h3 class="mb-3">New moderator (<a href="{{route('categories.show', $category)}}">{{$category->name}}</a>)</h3>
+        <h3 class="mb-3">New moderator ({{$category->name}})</h3>
 
         <form action="{{ route('moderators.store') }}" method="POST">
             <input type="hidden" name="category_id" value="{{$category->id}}">
