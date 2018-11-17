aa36c52b3bcf361c9f5bf315ad74bd30145667bd
plyschik <palyschik@gmail.com>
2018-11-17 22:03:33 +0100

    Routes cleanup.

--- routes/web.php
+++ routes/web.php
@@ -5,66 +5,136 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['verify' => true]);
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/{channel}/topic/create', 'TopicsController@create')->name('topics.create');
-    Route::post('/topic/create', 'TopicsController@store')->name('topics.store');
-    Route::post('/topic/{topic}', 'RepliesController@store')->name('replies.store');
-    Route::post('/replies/{reply}', 'RepliesController@storeResponse')->name('response.store');
-    Route::get('/replies/{reply}', 'RepliesController@createResponse')->name('response.create');
-    Route::get('/profile/{user}.html', 'UsersController@show')->name('users.show');
-    Route::get('/report/{reply}', 'ReportController@create')->name('report.create');
-    Route::post('/report/{reply}', 'ReportController@store')->name('report.store');
-});
-
-Route::group(['middleware' => 'can:manage,user'], function () {
-Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
-Route::get('users/{user}/edit','UsersController@edit')->name('users.edit');
-Route::patch('users/{user}','UsersController@update')->name('users.update');
-});
-
-Route::group(['middleware' => 'can:manage,topic'], function () {
-Route::get('topics/{topic}/edit','TopicsController@edit')->name('topics.edit');
-Route::patch('topics/{topic}','TopicsController@update')->name('topics.update');
-Route::delete('/topics/{topic}', 'TopicsController@destroy')->name('topics.destroy');
-});
-
-Route::group(['middleware' => 'can:manage,reply'], function () {
-Route::get('replies/{reply}/edit','RepliesController@edit')->name('replies.edit');
-Route::patch('replies/{reply}','RepliesController@update')->name('replies.update');
-Route::delete('/replies/{reply}', 'RepliesController@destroy')->name('replies.destroy');
-});
-
-Route::group(['middleware' => ['role:administrator']], function () {
-    Route::get('/channels/create', 'ChannelsController@create')->name('channels.create');
-    Route::post('/channels', 'ChannelsController@store')->name('channels.store');
-    Route::get('channels/{channel}/edit','ChannelsController@edit')->name('channels.edit');
-    Route::patch('channels/{channel}','ChannelsController@update')->name('channels.update');
-    Route::delete('/channels/{channel}', 'ChannelsController@destroy')->name('channels.destroy');
-    Route::get('/categories/create', 'CategoriesController@create')->name('categories.create');
-    Route::post('/categories', 'CategoriesController@store')->name('categories.store');
-    Route::get('categories/{category}/edit','CategoriesController@edit')->name('categories.edit');
-    Route::patch('categories/{category}','CategoriesController@update')->name('categories.update');
-    Route::delete('/categories/{category}', 'CategoriesController@destroy')->name('categories.destroy');
-    Route::get('/moderators/{category}/create', 'UsersController@createModerator')->name('moderators.create');
-    Route::post('/moderators', 'UsersController@storeModerator')->name('moderators.store');
-    Route::get('/moderators', 'UsersController@listModerators')->name('moderators.list');
-    Route::delete('/moderators/{user}/{category}', 'UsersController@destroyModerator')->name('moderators.destroy');
-});
-
-Route::group(['middleware' => ['role:administrator|moderator']], function () {
-    Route::get('/reports', 'ReportController@index')->name('report.index');
-    Route::get('/users/{user}/reports', 'ReportController@show')->name('report.show');
-    Route::get('/users/{user}/ban', 'BanController@create')->name('ban.create');
-    Route::post('/users/{user}/ban', 'BanController@store')->name('ban.store');
-    Route::post('/reports/{report}/ignore', 'ReportController@ignore')->name('report.ignore');
-    Route::post('/reports/{report}/delete', 'ReportController@delete')->name('report.delete');
-});
-
-
-Route::get('/channels', 'ChannelsController@index')->name('channels.index');
-Route::get('/channel/{channel}.html', 'ChannelsController@show')->name('channels.show');
-Route::get('/topic/{topic}.html', 'TopicsController@show')->name('topics.show');
-Route::view('/terms', 'pages.terms')->name('pages.terms');
-Route::view('/policy', 'pages.policy')->name('pages.policy');
-Route::get('/users/stats', 'UsersController@stats')->name('users.stats');
 Route::get('/', 'CategoriesController@index')->name('home');
+Route::get('/channel/{channel}.html', 'ChannelsController@show')->name('channels.show');
+
+Route::middleware('auth')->group(function () {
+    Route::post('topics/{topic}', 'RepliesController@store')->name('replies.store');
+    Route::get('/profile/{user}.html', 'UsersController@show')->name('users.show');
+});
+
+Route::name('response.')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/replies/{reply}', 'RepliesController@createResponse')->name('create');
+        Route::post('/replies/{reply}', 'RepliesController@storeResponse')->name('store');
+    });
+});
+
+Route::name('topics.')->group(function () {
+    Route::get('/topic/{topic}.html', 'TopicsController@show')->name('show');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/{channel}/topic/create', 'TopicsController@create')->name('create');
+    });
+});
+
+Route::name('report.')->group(function () {
+    Route::middleware('role:moderator|administrator')->group(function () {
+        Route::get('/reports', 'ReportController@index')->name('index');
+        Route::get('/users/{user}/reports', 'ReportController@show')->name('show');
+        Route::post('/reports/{report}/ignore', 'ReportController@ignore')->name('ignore');
+        Route::post('/reports/{report}/delete', 'ReportController@delete')->name('delete');
+    });
+});
+
+Route::name('ban.')->group(function () {
+    Route::middleware('role:moderator|administrator')->group(function () {
+        Route::get('/users/{user}/ban', 'BanController@create')->name('create');
+        Route::post('/users/{user}/ban', 'BanController@store')->name('store');
+    });
+});
+
+// categories
+Route::prefix('categories')->group(function () {
+    Route::name('categories.')->group(function () {
+        Route::middleware('role:administrator')->group(function () {
+            Route::get('create', 'CategoriesController@create')->name('create');
+            Route::post('', 'CategoriesController@store')->name('store');
+            Route::get('{category}/edit','CategoriesController@edit')->name('edit');
+            Route::patch('{category}','CategoriesController@update')->name('update');
+            Route::delete('{category}', 'CategoriesController@destroy')->name('destroy');
+        });
+    });
+});
+
+// moderators
+Route::prefix('moderators')->group(function () {
+    Route::name('moderators.')->group(function () {
+        Route::middleware('role:administrator')->group(function () {
+            Route::get('{category}/create', 'UsersController@createModerator')->name('create');
+            Route::post('', 'UsersController@storeModerator')->name('store');
+            Route::get('', 'UsersController@listModerators')->name('list');
+            Route::delete('{user}/{category}', 'UsersController@destroyModerator')->name('destroy');
+        });
+    });
+});
+
+// channels
+Route::prefix('channels')->group(function () {
+    Route::name('channels.')->group(function () {
+        Route::get('', 'ChannelsController@index')->name('index');
+
+        Route::middleware('role:administrator')->group(function () {
+            Route::get('create', 'ChannelsController@create')->name('create');
+            Route::post('', 'ChannelsController@store')->name('store');
+            Route::get('{channel}/edit','ChannelsController@edit')->name('edit');
+            Route::patch('{channel}','ChannelsController@update')->name('update');
+            Route::delete('{channel}', 'ChannelsController@destroy')->name('destroy');
+        });
+    });
+});
+
+// topics
+Route::prefix('topics')->group(function () {
+    Route::name('topics.')->group(function () {
+        Route::middleware('auth')->group(function () {
+            Route::post('create', 'TopicsController@store')->name('store');
+        });
+
+        Route::group(['middleware' => 'can:manage,topic'], function () {
+            Route::get('{topic}/edit','TopicsController@edit')->name('edit');
+            Route::patch('{topic}','TopicsController@update')->name('update');
+            Route::delete('{topic}', 'TopicsController@destroy')->name('destroy');
+        });
+    });
+});
+
+// replies
+Route::prefix('replies')->group(function () {
+    Route::name('replies.')->group(function () {
+        Route::middleware('can:manage,reply')->group(function () {
+            Route::get('{reply}/edit','RepliesController@edit')->name('edit');
+            Route::patch('{reply}','RepliesController@update')->name('update');
+            Route::delete('{reply}', 'RepliesController@destroy')->name('destroy');
+        });
+    });
+});
+
+// users
+Route::prefix('users')->group(function () {
+    Route::name('users.')->group(function () {
+        Route::get('stats', 'UsersController@stats')->name('stats');
+
+        Route::middleware('can:manage,user')->group(function () {
+            Route::get('{user}/edit','UsersController@edit')->name('edit');
+            Route::patch('{user}','UsersController@update')->name('update');
+            Route::delete('{user}', 'UsersController@destroy')->name('destroy');
+        });
+    });
+});
+
+// report
+Route::prefix('report')->group(function () {
+    Route::name('report.')->group(function () {
+        Route::middleware('auth')->group(function () {
+            Route::get('{reply}', 'ReportController@create')->name('create');
+            Route::post('{reply}', 'ReportController@store')->name('store');
+        });
+    });
+});
+
+// pages
+Route::name('pages.')->group(function () {
+    Route::view('/terms', 'pages.terms')->name('terms');
+    Route::view('/policy', 'pages.policy')->name('policy');
+});
